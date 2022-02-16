767e1866895e08ac8cb486c9728e646ed2f3c92c
kerryran <kerrigan.ranshaw@gmail.com>
2022-02-15 19:15:07 -0500

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BottomNavBarComponent } from './navigation/botnavbar.component';
+import { TopNavBarComponent } from './navigation/topnavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    BottomNavBarComponent
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/navigation/botnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ab-botnavbar',
+    templateUrl: 'botnavbar.component.html',
+    styleUrls: ['botnavbar.component.css']
+})
+export class BottomNavBarComponent {
+
+}
